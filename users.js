c274fe81605c5e88158a88eeac0d42b55ed0ad3f
Ian Isaacson <ian_isaacson@yahoo.com>
2025-05-24 00:05:08 -0700

    Exercise completed!

--- users.js
+++ users.js
@@ -1,5 +1,15 @@
 const users = [
-	{firstName: "Alice", lastName: "Johnson", points: 120},
-	{firstName: "Bob", lastName: "Smith", points: 99},
-	{firstName: "Charlie", lastName: "Brown", points: 180}
+  { firstName: "Alice", lastName: "Johnson", points: 120 },
+  { firstName: "Bob", lastName: "Smith", points: 99 },
+  { firstName: "Charlie", lastName: "Brown", points: 180 },
 ];
+
+console.log(
+  users.map(function (user) {
+    let membershipStatus = user.points > 100 ? "Premium" : "Standard";
+    return {
+      fullName: user.firstName + " " + user.lastName,
+      membershipStatus: membershipStatus,
+    };
+  })
+);
